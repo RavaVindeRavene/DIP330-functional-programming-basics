f7903da566a4be8c973cf3d19dbdf7536951dc7f
RavaVindeRavene <71387436+RavaVindeRavene@users.noreply.github.com>
2020-11-18 11:46:54 +0200

    Update ld3.clj

--- ld3.clj
+++ ld3.clj
@@ -30,7 +30,11 @@
 
 ;; 4. Reverse a list.
 (defn my-reverse [xs]
-  (into '() xs))
+  (if (empty? xs)
+   '()
+   (into '() xs)
+  )
+)
 
 (assert (= '(:d :c :b :a)
            (my-reverse '(:a :b :c :d))))
@@ -61,25 +65,35 @@
 
 ;; 7. Eliminate consecutive duplicates of a list.
 (defn compress [xs]
-  nil)
+ (distinct xs) ;; had no luck here
+)
 
 (assert (= '(:a :b :c)
            (compress '(:a :a :b :b :c :c))))
 
 ;; 8. Remove the N-th element of a list
 (defn remove-at [xs n]
-    (concat 
+    (when (seq xs)
+      (concat 
       (take n xs)
       (drop (inc n) xs)
-  )
+  ))
 )
+
 
 (assert (= '(:a :b :d :e)
            (remove-at '(:a :b :c :d :e) 2)))
 
 ;; 9. Insert a new element at the N-th position of a list.
 (defn insert-at [x xs n]
-  nil)
+   (when (seq xs)
+      (if (= n 0)
+        (conj xs x )
+        (concat (take n xs)  (conj (drop n xs) x))
+      )
+   )
+)
+
 
 (assert (= '(:a :b :x :c :d)
            (insert-at :x '(:a :b :c :d) 2)))
@@ -103,9 +117,11 @@
 
 ;; 12. Split a list into two parts; the length of the first part is given.
 (defn my-drop [xs n]
-  (if (= (- n 1) 0)
+  ( when (seq xs)
+    (if (= (- n 1) 0)
       (rest xs)
-      (recur (rest xs) (dec n)) 
+    (recur (rest xs) (dec n)) 
+    )
   )
 )
 
@@ -114,11 +130,14 @@
 
 ;; 13. Split a list into two parts; the length of the first part is given.
 (defn my-take [xs n]
-    (if (< n (count xs) )
-      (recur (butlast xs) n)
-    xs
-    )  
+   ( when (seq xs) 
+      (if (< n (count xs) )
+        (recur (butlast xs) n)
+      xs
+      )  
+    )
 )
+
 
 (assert (= '(:a :b :c)
            (my-take '(:a :b :c :d :e) 3)))
@@ -152,14 +171,25 @@
 
 ;; 16. Implement the reducing function
 (defn my-reduce [f acc xs]
-  nil)
+  (if (seq xs)
+	(f (first xs) (my-reduce f acc (next xs))
+    )acc)
+)
 
 (assert (= 15
            (my-reduce + 0 '(1 2 3 4 5))))
 
 ;; 17. Implement the flattening function
-(defn my-flatten [xs]
-  nil)
+(defn my-flatten [xs] 
+  (loop [list1 xs, list2 `()]
+    (cond (sequential? (first list1)) 
+      (recur (concat (first list1) (rest list1)) list2)
+        (empty? list1) 
+            (reverse list2)
+      :else (recur (rest list1) (cons (first list1) list2))
+    )
+  )
+)
 
 (assert (= '(:a :b :c :d :e)
            (my-flatten '(:a (:b (:c :d) :e)))))
