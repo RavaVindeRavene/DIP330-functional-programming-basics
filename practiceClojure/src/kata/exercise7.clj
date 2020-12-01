100b6022c2d3e42698d2af111c68dd89eb78bbf0
RavaVindeRavene <71387436+RavaVindeRavene@users.noreply.github.com>
2020-12-01 15:58:21 +0200

    upd1

---
+++ practiceClojure/src/kata/exercise7.clj
@@ -0,0 +1,22 @@
+(ns kata.exercise7
+  (:use [clojure.test]
+        [kata.data]))
+
+(testing "Average Age"
+
+  ; Calculate the average of customer ages by using your own (avg ...)
+  ;; Don't forget about (apply ...)
+  (let [customers (:customers mall)
+        avg-age ( )
+        ]
+
+    (is (= avg-age (rationalize 28.7)))))
+
+
+(testing "How Much for Everything?!"
+
+  ; Calculate the sum of the prices for all the items in all shops by using (flatten ...)
+  ;; Don't forget about (apply ...)
+  (let [total -1]
+
+    (is (= total 60930))))
