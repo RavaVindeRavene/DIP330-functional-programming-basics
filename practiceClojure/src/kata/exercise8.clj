1b7130f27e25b4f249b83b944b6f0fdd2b0c9bb7
RavaVindeRavene <71387436+RavaVindeRavene@users.noreply.github.com>
2020-12-08 20:39:25 +0200

    update8

--- practiceClojure/src/kata/exercise8.clj
+++ practiceClojure/src/kata/exercise8.clj
@@ -5,7 +5,11 @@
 (testing "Nowhere to be Found"
 
   ; Create a set of item names that are in {@link customer->wants-to-buy} but not on sale in any shop.
-  (let [not-for-sale #{}]
+  (let [
+        customers_want (set (map :name (mapcat :wants-to-buy (:customers mall))))
+        shops_have (set (map :name (mapcat :items (:shops mall))))
+        not-for-sale (set (remove shops_have customers_want))
+        ]
 
     (is (= not-for-sale #{"bag", "pants", "coat"}))))
 
@@ -14,6 +18,10 @@
   ; Create a customers' name list including who are having enough money to buy all items they want which is on sale.
   ; Items that are not for sale can be counted as 0 money cost.
   ; If there are multiple items with the same names, but different prices, customer will choose the cheapest one.
-  (let [richies []]
+  (let [
+        shops_have (mapcat :items (:shops mall))
+        richies []
+
+        ]
 
     (is (= richies ["Joe", "Patrick", "Chris", "Kathy", "Alice", "Andrew", "Amy"]))))
