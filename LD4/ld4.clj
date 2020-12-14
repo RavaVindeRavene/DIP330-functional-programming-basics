5b530d131b807ec98095e68ca83e9e4be922abc5
RavaVindeRavene <71387436+RavaVindeRavene@users.noreply.github.com>
2020-12-14 16:30:54 +0200

    Update ld4.clj

--- LD4/ld4.clj
+++ LD4/ld4.clj
@@ -2,18 +2,45 @@
 (require '[clojure.string :as str])
 
 (defn replace-space [message]
-  (str/replace message #" " "_")
+ 	(str/replace message #" " "_")
 )
 
 (defn char-valid [message key]
-  (if (re-matches #"^[A-Za-z\s_]+$" message)
-    (> key 1)
-    false)
+ 	(if (re-matches #"^[A-Za-z\s_]+$" message)
+		(> key 1)
+		false)
 )
+;length=count message
+;height=key
+(defn calculate-index [length height]
+	(def current-row 0)
+	(def go-row-down false)
+	(vec
+		(for [column (range 0 length)]
+			(do
+				(if (or(= current-row 0) (= current-row (- height 1)))
+					(def go-row-down (not go-row-down))
+				)
+				(def matrix-index (+ column (* current-row))
+				(def current-row
+					(if (true? go-row-down)
+						(inc current-row)
+						(dec current-row)
+					)
+				)
+				matrix-index
+			)
+		)
+	)
 
-;(defn calculate-index [length height])
+)
+;returns a vector/list of indexes and the chars of string
+; from [0 1 2 3 4 5] to something like [0 11 22 13 4 15]
+; this is key and chars of string is value? -> zipmap or for cycle to arrange that
 
 ;(defn do-encryption [message key])
+
+
 ;(defn do-decryption [message key])
 
 
