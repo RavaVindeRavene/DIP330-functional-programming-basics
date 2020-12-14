5e6ed29578d85e944d8a3c2b3fa8c97be4c5a4ec
RavaVindeRavene <71387436+RavaVindeRavene@users.noreply.github.com>
2020-12-14 15:00:19 +0200

    Update ld4.clj

--- LD4/ld4.clj
+++ LD4/ld4.clj
@@ -3,10 +3,29 @@
 
 (defn replace-space [message]
   (str/replace message #" " "_")
-  )
+)
 
 (defn char-valid [message key]
   (if (re-matches #"^[A-Za-z\s_]+$" message)
     (> key 1)
     false)
-  )
+)
+
+;(defn calculate-index [length height])
+
+;(defn do-encryption [message key])
+;(defn do-decryption [message key])
+
+
+;(defn encrypt [message key]
+	;(if (char-valid message key)
+		;(do-encryption (replace-space message) key)
+		; (str "Error: Invalid message or key!")
+	;)
+;)
+;(defn decrypt [message key]
+	;(if (char-valid message key)
+		;(do-decryption (replace-space message) key)
+		;(str "Error: Invalid message or key!")
+	;)
+;)
