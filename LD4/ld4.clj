6d1fe761f47877c27f09c5a12c021d724b1eaccb
RavaVindeRavene <71387436+RavaVindeRavene@users.noreply.github.com>
2020-12-14 18:11:17 +0200

    encryption works

--- LD4/ld4.clj
+++ LD4/ld4.clj
@@ -21,7 +21,7 @@
 				(if (or(= current-row 0) (= current-row (- height 1)))
 					(def go-row-down (not go-row-down))
 				)
-				(def matrix-index (+ column (* current-row))
+				(def matrix-index (+ column (* current-row length)))
 				(def current-row
 					(if (true? go-row-down)
 						(inc current-row)
@@ -38,18 +38,32 @@
 ; from [0 1 2 3 4 5] to something like [0 11 22 13 4 15]
 ; this is key and chars of string is value? -> zipmap or for cycle to arrange that
 
-;(defn do-encryption [message key])
+(defn zipmap-encrypt-chars [message key]
+	(zipmap 
+		(calculate-index(count message) key)( vec message)
+	)
+)
+
+(defn do-encryption [message key]
+	(str/join
+		(for [[new-index char]
+			(sort-by first (zipmap-encrypt-chars message key))
+			]
+			char
+		)	
+	)
+)
 
 
 ;(defn do-decryption [message key])
 
 
-;(defn encrypt [message key]
-	;(if (char-valid message key)
-		;(do-encryption (replace-space message) key)
-		; (str "Error: Invalid message or key!")
-	;)
-;)
+(defn encrypt [message key]
+	(if (char-valid message key)
+		(do-encryption (replace-space message) key)
+		(str "Error: Invalid message or key!")
+	)
+)
 ;(defn decrypt [message key]
 	;(if (char-valid message key)
 		;(do-decryption (replace-space message) key)
